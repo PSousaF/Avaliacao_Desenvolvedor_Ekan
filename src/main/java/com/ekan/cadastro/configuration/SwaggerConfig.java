67cf5e112699350d2c5515366a9bc756fa21a67c
Pedro Sousa Filho <81575097+PSousaF@users.noreply.github.com>
2024-04-08 22:40:13 -0300

    Projeto finalizado
    Projeto Ekan

--- src/main/java/com/ekan/cadastro/configuration/SwaggerConfig.java
+++ src/main/java/com/ekan/cadastro/configuration/SwaggerConfig.java
@@ -3,14 +3,12 @@ package com.ekan.cadastro.configuration;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
--- src/main/java/com/ekan/cadastro/service/BeneficiarioService.java
+++ src/main/java/com/ekan/cadastro/service/BeneficiarioService.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ekan.cadastro.model.Beneficiario;
 import com.ekan.cadastro.model.Documento;
